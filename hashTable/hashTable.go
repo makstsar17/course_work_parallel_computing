067fcc489498cf44847305e289215037a4335218
Макс Царьов <makstsar1701@gmail.com>
2023-01-15 14:20:01 +0200

    feat: add GetHashTable function

--- hashTable/hashTable.go
+++ hashTable/hashTable.go
@@ -16,3 +16,15 @@ type HashTable struct {
 	length   int
 	mu       sync.Mutex
 }
+
+func GetHashTable() *HashTable {
+	ht := HashTable{
+		buckets:  make([]*linkedList.LinkedList, 16),
+		capacity: 16,
+		length:   0,
+	}
+	for i := range ht.buckets {
+		ht.buckets[i] = &linkedList.LinkedList{Head: nil, Length: 0}
+	}
+	return &ht
+}
