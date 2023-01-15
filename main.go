d2f9b1139270e7959ea8b21e313d8c699637b439
Макс Царьов <makstsar1701@gmail.com>
2023-01-15 15:05:20 +0200

    feat: add main

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"server/invertedIndex"
+	"time"
+)
 
+func main() {
+	var threads int
+	flag.IntVar(&threads, "thr", 10, "Number of threads")
+	flag.Parse()
+
+	files, err := ioutil.ReadDir("dataset")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileNames := make([]string, len(files))
+	for i, f := range files {
+		fileNames[i] = "dataset/" + f.Name()
+	}
+
+	tm := time.Now()
+
+	ht := invertedIndex.IndexDocs(fileNames, threads)
+	//ht := invertedIndex.ConIndexDocs(fileNames)
+
+	fmt.Printf("[Time %.6fs] Threads number: %d\n", time.Since(tm).Seconds(), threads)
+
+	res, err := ht.Get("the")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key, value := range res {
+		fmt.Printf("the: %v: %d\n", key, value)
+	}
 }
