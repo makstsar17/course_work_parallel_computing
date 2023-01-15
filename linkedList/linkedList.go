0a6024e64f6aefc1688f15efec5b817028ef59df
Макс Царьов <makstsar1701@gmail.com>
2023-01-15 14:14:00 +0200

    feat: add insert functionality to LinkedList

--- linkedList/linkedList.go
+++ linkedList/linkedList.go
@@ -10,3 +10,28 @@ type LinkedList struct {
 	Head   *Node
 	Length int
 }
+
+func (l *LinkedList) Insert(word string, docId string) {
+	if l.Head == nil {
+		l.Head = &Node{Word: word, DocIds: make(map[string]int)}
+		l.Head.DocIds[docId] = 1
+		l.Length += 1
+		return
+
+	} else {
+		last := l.Head
+		for true {
+			if last.Word == word {
+				last.DocIds[docId] += 1
+				return
+			}
+			if last.Next == nil {
+				break
+			}
+			last = last.Next
+		}
+		last.Next = &Node{Word: word, DocIds: make(map[string]int)}
+		last.Next.DocIds[docId] = 1
+		l.Length += 1
+	}
+}
